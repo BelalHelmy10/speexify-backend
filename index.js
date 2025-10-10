870e41f0bbbca3e1d9cde22a83054f7c25eb324e
Belal <belal@example.com>
2025-10-11 01:06:56 +0300

    index.js updates for the port

--- index.js
+++ index.js
@@ -2136,6 +2136,7 @@ app.get("/api/db-check", async (_req, res) => {
 /* ========================================================================== */
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 5050;
-app.listen(PORT, () => {
-  console.log(`✅ Server is running on http://localhost:${PORT}`);
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`✅ Server is running on http://0.0.0.0:${PORT}`);
 });
