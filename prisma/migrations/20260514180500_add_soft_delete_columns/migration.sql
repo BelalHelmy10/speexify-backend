5d095fd8ca767379f64cd136d6698e84deabaf6a
Belal <belal@example.com>
2026-05-14 18:21:25 +0300

    some fixes

---
+++ prisma/migrations/20260514180500_add_soft_delete_columns/migration.sql
@@ -0,0 +1,7 @@
+ALTER TABLE "User"
+  ADD COLUMN "deletedAt" TIMESTAMP(3);
+
+ALTER TABLE "Package"
+  ADD COLUMN "deletedAt" TIMESTAMP(3);
+
+CREATE INDEX "User_deletedAt_idx" ON "User"("deletedAt");
